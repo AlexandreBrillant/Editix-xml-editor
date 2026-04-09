79225910a36343843f53d258d031ede66eed2f86
AlexandreBrillant <abrillant@japisoft.com>
2026-04-09 11:42:58 +0200

    build version fixed

--- src/com/japisoft/editix/main/steps/ConfigurationApplicationStep.java
+++ src/com/japisoft/editix/main/steps/ConfigurationApplicationStep.java
@@ -39,8 +39,8 @@ public class ConfigurationApplicationStep implements ApplicationStep {
 	static {
 		EditixApplicationModel.SHORT_APPNAME = "editix";
 		EditixApplicationModel.LONG_APPNAME = "Editix XML Editor";
-		EditixApplicationModel.BUILD = "010625";
-		EditixApplicationModel.INNER_BUILD = "010625";
+		EditixApplicationModel.BUILD = "090426";
+		EditixApplicationModel.INNER_BUILD = "090426";
 		EditixApplicationModel.BETA_VERSION = 0;
 		EditixApplicationModel.MAJOR_VERSION = 21;
 		
